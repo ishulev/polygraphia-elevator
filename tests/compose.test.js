c8634c999a7e8730a079ae7cc345f8448164bbb8
Ivan Shulev <ishulev@melontech.com>
2017-11-02 15:13:04 +0200

    add tests for compose

--- tests/compose.test.js
+++ tests/compose.test.js
@@ -6,3 +6,21 @@ test('compose should take only functions for arguments', function (t) {
     t.doesNotThrow(compose(function(){}, function(){}));
     t.end();
 });
+
+test('compose should return a function', function (t) {
+    t.is(typeof compose(), 'function');
+    t.end();
+});
+
+test('compose should return true if all the passed functions return true', function (t) {
+    t.is(compose(function(){}, function(){})(), false);
+    t.is(compose(function(){return false;}, function(){return true;})(), false);
+    t.is(compose(function(){return true;}, function(){return true;})(), true);
+    t.end();
+});
+
+test('compose should return true if all the passed functions return true, given a parameter', function (t) {
+    t.is(compose(function(param){ return param; }, function(param){ return param;})(true), true);
+    t.is(compose(function(param){ return !param; }, function(param){ return param;})(true), false);
+    t.end();
+});
