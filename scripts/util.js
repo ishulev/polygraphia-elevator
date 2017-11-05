bb52b53a46ec70d78a1a89c21b64189b8e669a84
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-11-05 13:23:07 +0200

    change naming

--- scripts/util.js
+++ scripts/util.js
@@ -15,18 +15,18 @@ module.exports = function pipe() {
     // Make array-like 'arguments' into an array
     // This needs Array.prototype, because arguments is not an array
     // so I guess coersion happens
-    var composerArguments = Array.prototype.slice.call(arguments);
+    var pipeArguments = Array.prototype.slice.call(arguments);
     if(!allFunctionArguments) {
         throw 'Not all arguments are functions!';
     }
 
     return function(valueToCheck) {
-        return composerArguments.reduce(
+        return pipeArguments.reduce(
             function (accumulator, individualFunction) {
                 return accumulator + individualFunction.call(
                     null, valueToCheck
                 );
             },
-        0) === composerArguments.length;
+        0) === pipeArguments.length;
     }
 };
